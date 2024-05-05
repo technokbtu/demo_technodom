960b2210d19c5884643cc2c7d52608a42b3bfd0c
tanqurai <higashigatajosuke16@gmail.com>
2024-05-05 14:02:52 +0600

    alikhan:improved adaptivka

--- draft/components_draft/js/burger.js
+++ draft/components_draft/js/burger.js
@@ -1,6 +1,11 @@
-// burger.js
-console.log('hello');
-const burger_button = document.querySelector('.burger-menu');
-burger_button.addEventListener('click', () => {
-    console.log('click!');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('hello');
+    const burger_button = document.querySelector('.burger-menu');
+    const burger_info = document.querySelector('.burger-info');
+    let opened = false;
+    burger_button.addEventListener('click', () => {
+        opened = !opened;
+        console.log('click');
+        burger_info.style.display = opened?'flex':'none';
+    });
 });
